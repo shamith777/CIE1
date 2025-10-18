db1d328f81e7a290c2c872bf665f7d6e23089473
shamith777 <shamithramdurg302@gmail.com>
2025-10-18 12:24:13 +0530

    Saving Modified File

--- cie.py
+++ cie.py
@@ -1,4 +1,7 @@
 balance = float(input("Enter your balance: "))
 deposit = float(input("Enter deposit amount: "))
 new_balance = balance + deposit
-print("The updated account balance is:", new_balance)
+print("The updated account balance:", new_balance)
+withdrawal = float(input("Enter withdrawal amount: "))
+updated_balance = new_balance - withdrawal
+print("The updated account balance after withdrawal:", updated_balance)
